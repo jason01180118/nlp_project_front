23b742915498bfd01177515a695e4769097a973b
jason01180118 <73685101+jason01180118@users.noreply.github.com>
2023-04-09 12:21:45 +0800

    feat: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "eslint.autoFixOnSave": true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
